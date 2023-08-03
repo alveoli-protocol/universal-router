1548f420c540301f35d60a996ae3442e176c5abe
Luciflow <luciflow@proton.me>
2023-08-03 18:19:24 +0000

    Fix DeployCypress script

--- script/deployParameters/DeployCypress.s.sol
+++ script/deployParameters/DeployCypress.s.sol
@@ -7,8 +7,8 @@ import {RouterParameters} from 'contracts/base/RouterImmutables.sol';
 contract DeployCypress is DeployUniversalRouter {
     function setUp() public override {
         params = RouterParameters({
-            permit2: UNSUPPORTED_PROTOCOL,
-            weth9: 0x043c471bEe060e00A56CcD02c0Ca286808a5A436,
+            permit2: 0xB952578f3520EE8Ea45b7914994dcf4702cEe578,
+            weth9: 0x19Aac5f612f524B754CA7e7c41cbFa2E981A4432,
             seaportV1_5: UNSUPPORTED_PROTOCOL,
             seaportV1_4: UNSUPPORTED_PROTOCOL,
             openseaConduit: UNSUPPORTED_PROTOCOL,
@@ -29,6 +29,6 @@ contract DeployCypress is DeployUniversalRouter {
             poolInitCodeHash: 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54
         });
 
-        unsupported = 0x0000000000000000000000000000000000000000;
+        unsupported = 0x8b41Ab5383fD4c007E1FAC4080e38acAbC778e93;
     }
 }
